ce57c317e889900fe3d148a4c12167af7214f950
Alex Karalanian <alex.karalanian@gmail.com>
2019-12-10 14:50:40 -0500

    init commit

---
+++ src/test/java/com/alexkaralanian/FullstackSpringboot/FullstackSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexkaralanian.FullstackSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
